000b4c5a6b3c5bd0f0c283854e12e4d5177e8fa8
Reagan <mamesareagan0@gmail.com>
2024-03-04 18:52:43 +0300

    island perimeter

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Island Perimeter """
+
+
+def island_perimeter(grid):
+    """ Island Perimeter """
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                if i == 0 or not grid[i - 1][j]:
+                    perimeter += 1
+                if i == len(grid) - 1 or not grid[i + 1][j]:
+                    perimeter += 1
+                if j == 0 or not grid[i][j - 1]:
+                    perimeter += 1
+                if j == len(grid[i]) - 1 or not grid[i][j + 1]:
+                    perimeter += 1
+    return perimeter
